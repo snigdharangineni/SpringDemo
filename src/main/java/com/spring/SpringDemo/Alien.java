1565509d87dae40efbdbea0f836067d6531184c5
snigdha <sunny.rangineni@gmail.com>
2024-06-07 15:53:59 -0500

    Assigning object ref to property tag in bean tag

--- src/main/java/com/spring/SpringDemo/Alien.java
+++ src/main/java/com/spring/SpringDemo/Alien.java
@@ -1,12 +1,17 @@
 package com.spring.SpringDemo;
 
-
-import org.springframework.stereotype.Component;
-
-@Component
 public class Alien {
 	private int age;
+	private Laptop laptop;
 	
+	public Laptop getLaptop() {
+		return laptop;
+	}
+
+	public void setLaptop(Laptop laptop) {
+		this.laptop = laptop;
+	}
+
 	public Alien() {
 		System.out.println("In alien Constructor");
 	}
@@ -21,5 +26,6 @@ public class Alien {
 	public void code()
 	{
 		System.out.println("In coding");
+		laptop.compile();
 	}
 }
---
+++ src/main/java/com/spring/SpringDemo/Laptop.java
@@ -0,0 +1,9 @@
+package com.spring.SpringDemo;
+
+public class Laptop {
+
+	public void compile()
+	{
+		System.out.println("compiling");
+	}
+}
